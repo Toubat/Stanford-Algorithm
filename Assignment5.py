c58bc86869a1597b84495cd7c707c3710f795c44
Toubat <57741529+Toubat@users.noreply.github.com>
2020-12-30 04:47:59 +0800

    save assignment 5

--- Assignment5.py
+++ Assignment5.py
@@ -1,27 +1,59 @@
 # Assignment 5
 from collections import deque
 
-class Graph:
+# global variables of finishing time - t and most recent source vertex - src
+time = 0
+src = 0
 
-    def __init__(self, input_list):
-        self.vertices = {}
+class DirectedGraph:
+
+    def __init__(self, input_list, reverse=False):
+        self.graph = {}
         self.num_vertices = 0
-        self.constructGraph(input_list)
+        self.constructGraph(input_list, reverse)
+        self.orders = [0 for _ in range(self.num_vertices + 1)]
+        self.explored = [False for _ in range(self.num_vertices + 1)]
 
-    def constructGraph(self, input_list):
-        # input is a 2D list, where the first element of each row is a vertex, followed by nodes connected to it
-        self.num_vertices = len(input_list)
-        for row in input_list:
-            vertex = row[0]
-            connected_nodes = row[1:]
-            self.vertices[vertex] = connected_nodes
+    def constructGraph(self, input_list, reverse=False):
+        self.graph = {}
+
+        for edge in input_list:
+            if reverse:
+                head, tail = edge[0], edge[1]
+            else:
+                tail, head = edge[0], edge[1]
+            if tail in self.graph:
+                self.graph[tail].append(head)
+            else:
+                self.graph[tail] = [head]
+                self.num_vertices += 1
+            if head not in self.graph:
+                self.graph[head] = []
+                self.num_vertices += 1
 
         return 0
 
     def __str__(self):
         info = f"Graph with {self.num_vertices} vertices\n"
+        for i in range(1, 11):
+            info += f"Vertex {i} : {str(self.graph[i])}\n"
 
-        return info + str(self.vertices)
+        return info
+
+    def DFS(self, start):
+        self.explored[start] = True
+        self.orders[start] = src
+        stack = deque()
+        stack.append(start)
+        while len(stack):
+            v = stack.pop()
+            #for u in self.graph[v]
+
+
+        return 0
+
+    def computeSSCs(self):
+        pass
 
 
 def initInput(lst, path):
@@ -39,9 +71,9 @@ def initInput(lst, path):
 
 def main():
     lst = []
-    initInput(lst, "assign5_input.txt")
-    print(len(lst))
-    print(lst[:20])
+    initInput(lst, 'assign5_input.txt')
+    graph = DirectedGraph(lst, reverse=False)
+    print(graph)
 
     return 0
 
